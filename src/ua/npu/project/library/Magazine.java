b3b2fc83da04587ef7ff8c64fd1a492b639667c2
korsunl <korsunartemka@gmail.com>
2016-02-02 10:13:08 +0200

    новий клас

--- src/ua/npu/project/library/Magazine.java
+++ src/ua/npu/project/library/Magazine.java
@@ -1,9 +1,37 @@
 package ua.npu.project.library;
 
-/**
- * Created by student229a on 30.01.2016.
- */
-public class Magazine extends Publication {
-    private int Number();
 
+//Журнал
+
+
+public class Magazine extends Publication {
+    public int pages;
+
+    public Magazine(int pages) {
+        this.pages = pages;
+    }
+
+    public Magazine(String name, int year, String autor, int pages) {
+        super(name, year, autor);
+        this.pages = pages;
+    }
+
+    public Magazine() {
+        this.pages = 0;
+    }
+
+    public Magazine(String name, int year, String autor) {
+        super(name, year, autor);
+        this.pages = 0;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. T
